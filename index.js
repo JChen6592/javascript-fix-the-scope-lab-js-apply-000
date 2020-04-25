62e535624f15f45438ad052eab1afc8df7aa1670
John Chen <johnchen062094@gmail.com>
2020-04-25 18:01:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ function myAnimal() {
 function yourAnimal() {
   // The tests expect this function to return `animal` just like the previous function
   // However, you cannot simply modify the existing variable declared on line 1 in the global scope 
-  // How can we make sure that this functionl
+  // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
-  yourAnimal("cat")
+  var animal = "cat"
   return animal
 }
 
